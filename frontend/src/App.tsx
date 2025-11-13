2a303c40e26dd0cdd8d197bf0b7fab4619b8ac57
Pawel <pawmag.code@gmail.com>
2025-11-13 20:54:18 +0100

    Add basic pages structure (Dashboard, Alerts)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import DashboardPage from "./pages/DashboardPage";
+
 function App() {
   return (
     <div>
       <h1>PDCA Alert Dashboard</h1>
-      <p>Tu zbudujemy interfejs do obsługi alertów i cyklu PDCA.</p>
+      <DashboardPage />
     </div>
   );
 }
